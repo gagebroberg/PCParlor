2670f26b2ee1d0aaefaf15ebe0cb1252c52121db
gagebroberg <64049160+gagebroberg@users.noreply.github.com>
2022-04-02 21:37:47 -0500

    Adding gpu logic

--- index.js
+++ index.js
@@ -10,9 +10,21 @@ express()
   .get('/cpu', async (req, res) => {
     try {
       const client = await pool.connect();
-      const result = await client.query('SELECT * FROM comp_parts');
+      const result = await client.query("SELECT * FROM comp_parts WHERE type='CPU'");
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/cpu', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/gpu', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM comp_parts WHERE type='GPU'");
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/gpu', results );
       client.release();
     } catch (err) {
       console.error(err);
@@ -23,8 +35,6 @@ express()
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connectionString: process.env.DATABASE_URL || 'postgres://hmzgcfxxfupocw:db0051069cbafc30ce2a903328d4611dc7d8da5f385da1113043067f297ba1b5@ec2-18-215-96-22.compute-1.amazonaws.com:5432/d5c9oa1b7747vs',
+  ssl: process.env.DATABASE_URL ? true : false
 });
