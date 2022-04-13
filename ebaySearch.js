5a97b461dceb7d54fb637d3a05a9424f866785e7
JacobDPoland <45803275+JacobDPoland@users.noreply.github.com>
2022-04-13 17:17:20 -0500

    Parsed eBay response into json

--- ebaySearch.js
+++ ebaySearch.js
@@ -1,14 +1,6 @@
 
-// Variables
-const { resolveInclude } = require('ejs');
 const fetch = require('node-fetch')
-const xml2js = require('xml2js');
-
-
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// Functions
-
+const xml2js = require('xml2js')
 
 
 async function ebaySearch(search_term){
@@ -20,11 +12,10 @@ async function ebaySearch(search_term){
         'X-EBAY-SOA-OPERATION-NAME': 'findItemsAdvanced'
     };
 
-        
     let ebayReqBody = `
     <findItemsAdvancedRequest xmlns="http://www.ebay.com/marketplace/search/v1/services">
-    <categoryId> 175673 </categoryId>
-    <keywords> ${search_term} </keywords>
+        <categoryId> 175673 </categoryId>
+        <keywords> ${search_term} </keywords>
     </findItemsAdvancedRequest>
     `;
     
@@ -39,7 +30,10 @@ async function ebaySearch(search_term){
     }
 
     const response = await fetch(ebayURI, ebayParams);
-    const text = await response.text();
-    console.log(text);
+    const xml = await response.text();
+    
+    let parser = new xml2js.Parser();
+    return parser.parseStringPromise(xml);  // response is a promise that will resolve to json
 }
 module.exports.ebaySearch = ebaySearch;
+
--- ebaySearchTest.js
+++ ebaySearchTest.js
@@ -1,2 +1,16 @@
+// const fetch = require('node-fetch')
 const ebaySearch = require('./ebaySearch.js').ebaySearch;
-console.log(ebaySearch("rtx 2060"));
+
+ebaySearch("rtx 2060")
+.then((response) => {
+    console.log("successful eBay search call");
+    let listings = response['findItemsAdvancedResponse']['searchResult'][0]['item'];
+
+    
+    let two_items = listings.slice(0, 2);
+    console.log(two_items);
+
+})
+.catch((err) => {
+    console.error(err);
+});
