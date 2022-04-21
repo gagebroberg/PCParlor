47811c1cb5481ef920a59b1dc4420095282c8f21
gagebroberg <64049160+gagebroberg@users.noreply.github.com>
2022-04-20 23:04:31 -0500

    Preparing Sprint 2 Release

---
+++ ebaySearch.js
@@ -0,0 +1,41 @@
+const fetch = require('node-fetch')
+const xml2js = require('xml2js')
+
+
+async function ebaySearch(search_term, page){
+    let ebayURI = "https://svcs.ebay.com/services/search/FindingService/v1";
+
+    let ebayReqHeaders = 
+    {
+        'X-EBAY-SOA-SECURITY-APPNAME': 'JacobPol-pcparlor-PRD-cdc51ba6c-43b0d70a',
+        'X-EBAY-SOA-OPERATION-NAME': 'findItemsAdvanced'
+    };
+
+    let ebayReqBody = `
+    <findItemsAdvancedRequest xmlns="http://www.ebay.com/marketplace/search/v1/services">
+        <categoryId> 175673 </categoryId>
+        <keywords> ${search_term} </keywords>
+        <paginationInput>
+            <entriesPerPage>100</entriesPerPage>
+            <pageNumber> ${page} </pageNumber>
+        </paginationInput>
+    </findItemsAdvancedRequest>
+    `;
+    
+    const ebayParams = {    
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/xml',
+            'X-EBAY-SOA-SECURITY-APPNAME': 'JacobPol-pcparlor-PRD-cdc51ba6c-43b0d70a',
+            'X-EBAY-SOA-OPERATION-NAME': 'findItemsAdvanced'
+        },
+        body: ebayReqBody
+    }
+
+    const response = await fetch(ebayURI, ebayParams);
+    const xml = await response.text();
+    
+    let parser = new xml2js.Parser();
+    return parser.parseStringPromise(xml);  // response is a promise that will resolve to json
+}
+module.exports.ebaySearch = ebaySearch;
--- index.js
+++ index.js
@@ -1,10 +1,231 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const { range } = require('express/lib/request');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const ebaySearch = require('./ebaySearch.js').ebaySearch;
+const { Pool } = require('pg');
+const { ConsoleMessage } = require('puppeteer');
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+    }
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/', async (req, res) => {
+    res.render('pages/index');
+  })
+  .get('/search', async (req, res) => {
+    try {
+      // TODO: Dumps into cpu route right now no matter what. Probably want search route to display "Products". Also only searches model name.
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM cpu WHERE LOWER(model) LIKE LOWER('%" + req.query['searchquery'] + "%') UNION "
+                                      + "SELECT * FROM gpu WHERE LOWER(model) LIKE LOWER('%" + req.query['searchquery'] + "%') UNION "
+                                      + "SELECT * FROM ssd WHERE LOWER(model) LIKE LOWER('%" + req.query['searchquery'] + "%') UNION "
+                                      + "SELECT * FROM hdd WHERE LOWER(model) LIKE LOWER('%" + req.query['searchquery'] + "%') UNION "
+                                      + "SELECT * FROM ram WHERE LOWER(model) LIKE LOWER('%" + req.query['searchquery'] + "%') UNION "
+                                      + "SELECT * FROM usb WHERE LOWER(model) LIKE LOWER('%" + req.query['searchquery'] + "%') " 
+                                      + "ORDER BY rank");
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/search', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/cpu', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM cpu");
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/cpu', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/gpu', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM gpu");
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/gpu', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/ssd', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM ssd");
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/ssd', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/hdd', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM hdd");
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/hdd', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/ram', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM ram");
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/ram', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/usb', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query("SELECT * FROM usb");
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/usb', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/signin', async (req, res) => {
+    try {
+      res.render('pages/signin');
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/signup', async (req, res) => {
+    try {
+      res.render('pages/signup');
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/about', async (req, res) => {
+    try {
+      res.render('pages/about');
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/howto', async (req, res) => {
+    try {
+      res.render('pages/howto');
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/product/:search', async (req, res) => {
+    try {
+      let searchTerm = req.params['search'];
+      let ebayData = new Array();
+      ebaySearch(searchTerm, 1)
+      .then((searchResponse) => {
+        let listings = searchResponse['findItemsAdvancedResponse']['searchResult'][0]['item'];
+        
+        for (let itemnum = 0; itemnum < 100; itemnum++) {
+          let item = listings[itemnum];
+          if (item == undefined) {
+            break;
+          }
+          let title = item['title'][0];
+          let picture = item['galleryURL'][0];
+          let link = item['viewItemURL'][0];
+          let location = item['location'][0];
+          let country = item['country'][0];        
+          let sellingStatus = item['sellingStatus'];
+          let price = sellingStatus[0]['currentPrice'][0]['_']
+          let currency = sellingStatus[0]['currentPrice'][0]['$']['currencyId'];
+
+          ebayData[itemnum] = {
+            'title': title,
+            'picture': picture,
+            'link': link,
+            'location': location,
+            'country': country,
+            'price': price,
+            'currency': currency
+          }
+        }
+        res.render('pages/product', {'data': ebayData, 'itemCount': ebayData.length, 'searchTerm': searchTerm});
+      })
+      .catch((err) => {
+        throw err;
+      });
+    
+      } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .get('/details/:search', async (req, res) => {
+    try {
+      let searchTerm = req.params['search'];
+      let ebayData = new Array();
+      ebaySearch(searchTerm, 1)
+      .then((searchResponse) => {
+        let listings = searchResponse['findItemsAdvancedResponse']['searchResult'][0]['item'];
+        
+        for (let itemnum = 0; itemnum < 100; itemnum++) {
+          let item = listings[itemnum];
+          if (item == undefined) {
+            break;
+          }
+          let title = item['title'][0];
+          let picture = item['galleryURL'][0];
+          let link = item['viewItemURL'][0];
+          let location = item['location'][0];
+          let country = item['country'][0];        
+          let sellingStatus = item['sellingStatus'];
+          let price = sellingStatus[0]['currentPrice'][0]['_']
+          let currency = sellingStatus[0]['currentPrice'][0]['$']['currencyId'];
+
+          ebayData[itemnum] = {
+            'title': title,
+            'picture': picture,
+            'link': link,
+            'location': location,
+            'country': country,
+            'price': price,
+            'currency': currency
+          }
+        }
+        res.render('pages/details', {'data': ebayData, 'itemCount': ebayData.length, 'searchTerm': searchTerm});
+      })
+      .catch((err) => {
+        throw err;
+      });
+    
+      } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
